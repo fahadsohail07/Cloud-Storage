893f4ebafb0cd3cbbc1636f970477bce6ea36e7b
fahadsohail_07 <fahadsohail08@gmail.com>
2024-04-01 23:13:42 +0500

    commit

--- backend/IdntyAccMgmtServ/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IdntyaccmgmtservConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'IdntyAccMgmtServ'
--- backend/StorageMgmtServ/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import StorageFile,Folder
-
-
-admin.site.register(StorageFile)
-admin.site.register(Folder)
-
-
-# Register your models here.
